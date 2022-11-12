7588bdd1e4214483c4d1dab61a6718f2c4dcd26f
irsyadnb <irsyadxd@gmail.com>
2022-11-12 17:02:03 +0700

    revisi cof matrix

--- main.py
+++ main.py
@@ -2,77 +2,52 @@ import cv2, os, numpy
 #kalo nemu ds_store gjls
 #find . -name ".DS_Store" -delete
 ctr = 0
-for folder in os.listdir("Algeo02-21072/setdata/"):
-    for imgfile in os.listdir("Algeo02-21072/setdata/"+folder):
+for folder in os.listdir("Algeo02-21072/testdata/"):
+    for imgfile in os.listdir("Algeo02-21072/testdata/"+folder):
         ctr+=1
 
 def setdata(foldername):
     images = []
-    print(numpy.shape(images))
-
     for folder in os.listdir(foldername):
         for imgfile in os.listdir(foldername+folder):
             img = cv2.imread(os.path.join(foldername+folder, imgfile), cv2.IMREAD_GRAYSCALE)
             img = cv2.resize(img,(256,256))
-            images.append(img.reshape(-1,1))
-
+            images.append(img.reshape(-1,1)) 
+            #vector face dibuat dari 256x256 jadi 65536 x M 
+            #dimasukan ke face vector space (images)
+    
     return images
-
-#setdata("Algeo02-21072/dataset/")  
+#setdata("Algeo02-21072/testdata/")  
 
 def average(arr):
     sum = [[0 for i in range(1)] for j in range(256*256)]
+    a = []
     for i in range(ctr):
         sum = numpy.array(numpy.add(sum, arr[i]))
     mean = numpy.array(numpy.divide(sum, ctr))
+
+    for j in range(ctr): #matrix a diisi normalized vector (tiap vector face - mean)
+        a.append(numpy.array(numpy.subtract(arr[j], mean)))
+    #hasil matrix a -> N^2 x M
+
+    #buat hasilin average face
     box = int(numpy.sqrt(mean.shape[0]))
     cv2.imwrite("WOIPRINTWOI.jpg", numpy.reshape(mean,(box,box)))
-    return mean
+    gambar = numpy.array(a).reshape(256*256,-1)
+    return gambar #ngehasilin matrix isi normalized vectors (A)
 
-#average(setdata("Algeo02-21072/dataset/"))
+#average(setdata("Algeo02-21072/testdata/"))
 
-def cofmatrix(apakek):
-    normal = apakek
-    transpose = numpy.transpose(apakek)
-    cof = numpy.multiply(normal, transpose)
-    print(numpy.shape(numpy.array(cof)))
-    return cof
+def cofmatrix(normalvector):
+    normal = normalvector
+    transpose = numpy.transpose(normal)
+    cof = numpy.matmul(transpose, normal)
+    return cof #eigen vectors
 
-cofmatrix(average(setdata("Algeo02-21072/setdata/")))
+def cofmatrixbig(normalvector):
+    normal = normalvector
+    transpose = numpy.transpose(normal)
+    cof = numpy.matmul(normal, transpose)
+    return cof #eigen vectors versi gede
 
-
-#nyimpen
-'''
-def dataset(foldername):
-    images = []
-    
-    sum = [[0 for i in range(256)] for j in range(256)]
-    for folder in os.listdir(foldername):
-        for imgfile in os.listdir(foldername+folder):
-            img = cv2.imread(os.path.join(foldername+folder, imgfile), cv2.IMREAD_GRAYSCALE)
-            img = cv2.resize(img,(256,256))
-            images.append(img)
-            sum = numpy.add(sum, img)
-    mean = numpy.array(numpy.divide(sum, len(images)))
-    image = numpy.array(images)
-    cv2.imwrite('avg.jpg', mean)
-
-    a = []
-
-    for i in range(len(images)):
-        a.append(numpy.array(numpy.subtract(image[i], mean)))
-    
-    return(numpy.array(a))
-    
-#dataset("Algeo02-21072/setdata/")
-
-def cofmatrix(a):
-    normal = a
-    transpose = numpy.transpose(a)
-    print("normal :")
-    print(numpy.array(normal).shape)
-    print("transpose:")
-    print(numpy.array(transpose).shape)
-
-#cofmatrix(dataset("Algeo02-21072/dataset/"))
-'''
+#cofmatrix(average(setdata("Algeo02-21072/testdata/")))
